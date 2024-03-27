d20816584f6568c384ec99e7008ef399e79dcc91
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-03-26 21:46:55 -0500

    Feat: Dynamically add giveaway date

--- 12-countdown/app.js
+++ 12-countdown/app.js
@@ -21,3 +21,29 @@ const weekdays = [
   'Friday',
   'Saturday'
 ]
+
+const giveaway = document.querySelector('.giveaway')
+const deadline = document.querySelector('.deadline')
+const deadlineHeaders = document.querySelectorAll('.deadline-format h4')
+
+const giveawayDate = new Date('July 3, 2024 11:30:00')
+
+const giveawayYear = giveawayDate.getFullYear()
+const giveawayDay = giveawayDate.getDate()
+const giveawayHours = giveawayDate.getHours()
+const giveawayMinutes = giveawayDate.getMinutes()
+// const giveawaySeconds = giveawayDate.getSeconds()
+
+const giveawayMonth = months[giveawayDate.getMonth()]
+const giveawayWeekday = weekdays[giveawayDate.getDay()]
+
+let giveawayAmPm
+if (giveawayHours >= 13) {
+	giveawayAmPm = 'pm'
+} else {
+	giveawayAmPm = 'am'
+}
+
+giveaway.textContent = `giveaway ends on ${giveawayWeekday}, ${giveawayMonth} ${giveawayDay} ${giveawayYear} ${giveawayHours}:${giveawayMinutes}${giveawayAmPm}`
+
+console.log(giveawayWeekday)
