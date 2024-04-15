5d02a1f039419f1a59f1a4973142b75c4fd7fb67
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-04-14 22:58:49 -0500

    Feat: Add preloader

--- 09-video/app.js
+++ 09-video/app.js
@@ -1,3 +1,9 @@
+const preloader = document.querySelector('.preloader')
+
+window.addEventListener('load', function() {
+	preloader.classList.add('hide-preloader')
+})
+
 const btn = document.querySelector('.switch-btn')
 const video = document.querySelector('.video-container')
 
