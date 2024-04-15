6c49c9d4fb3aded5902b10ebec29c697b4d43580
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-04-14 22:58:11 -0500

    Fix: Pause and play video were reversed

--- 09-video/app.js
+++ 09-video/app.js
@@ -4,9 +4,9 @@ const video = document.querySelector('.video-container')
 btn.addEventListener('click', function() {
 	if (!btn.classList.contains('slide')) {
 		btn.classList.add('slide')
-		video.play()
+		video.pause()
 	} else {
 		btn.classList.remove('slide')
-		video.pause()
+		video.play()
 	}
 })
