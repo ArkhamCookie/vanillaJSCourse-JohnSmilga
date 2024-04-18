b3e287c1c4f725c01a6f713cfbb23cbff3f89d39
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-04-17 21:13:21 -0500

    Feat: Use simular method to menu project to display projects
    Using menu project's css for now,
    but that will be replaced with css better suited for the project

---
+++ public/app.js
@@ -0,0 +1,31 @@
+const projects = [
+	{
+		title: 'color flipper',
+		img: './images/color-flipper.png'
+	},
+	{
+		title: 'Counter',
+		img: './images/counter.png'
+	}
+]
+
+const sectionCenter = document.querySelector('.section-center')
+
+function displayItems(projects) {
+	const displayProjects = projects.map(function (item) {
+
+		return `<article class="preview">
+				<img src="${item.img}" class="preview-image" alt="${item.title} preview">
+				<div class="item-info">
+					<h3>${item.title}</h3>
+				</div>
+			</article>`
+	}).join('')
+
+	sectionCenter.innerHTML = displayProjects
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+	displayItems(projects)
+	displayMenuButtons()
+})
