7b68170bbe685dcf166413190cd779ae95f4c205
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-04-27 23:40:18 -0500

    Feat: Add all projects

--- public/app.js
+++ public/app.js
@@ -6,6 +6,58 @@ const projects = [
 	{
 		title: 'Counter',
 		img: './images/counter.png'
+	},
+	{
+		title: 'Reviews',
+		img: './images/reviews.png'
+	},
+	{
+		title: 'Navbar',
+		img: './images/navbar.png'
+	},
+	{
+		title: 'Sidebar',
+		img: './images/sidebar.png'
+	},
+	{
+		title: 'Modal',
+		img: './images/modal.png'
+	},
+	{
+		title: 'Questions',
+		img: './images/questions.png'
+	},
+	{
+		title: 'Menu',
+		img: './images/menu.png'
+	},
+	{
+		title: 'Video',
+		img: './images/video.png'
+	},
+	{
+		title: 'Scroll',
+		img: './images/scroll.png'
+	},
+	{
+		title: 'Tabs',
+		img: './images/tabs.png'
+	},
+	{
+		title: 'Countdown',
+		img: './images/countdown.png'
+	},
+	{
+		title: 'Lorem Ipsum',
+		img: './images/lorem-ipsum.png'
+	},
+	{
+		title: 'Grocery Bud',
+		img: './images/grocery-bud.png'
+	},
+	{
+		title: 'Slider',
+		img: './images/slider.png'
 	}
 ]
 
