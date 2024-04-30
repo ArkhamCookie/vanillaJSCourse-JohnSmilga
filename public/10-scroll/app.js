11748233aa964310ab944372fd1f4b84dfb5e972
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-04-29 21:55:05 -0500

    Feat: Add toggle links button functionality
    Button seems to not be showing up.
    This is true in John's example of the project too.
    I will have to look into this more later.

--- public/10-scroll/app.js
+++ public/10-scroll/app.js
@@ -2,3 +2,12 @@
 const date = document.getElementById('date')
 
 date.innerHTML = new Date().getFullYear()
+
+/** Close Links */
+const navToggle = document.querySelector('.nav-toggle')
+const linksContainer = document.querySelector('.links-container')
+const links = document.querySelector('.links')
+
+navToggle.addEventListener('click', function() {
+	linksContainer.classList.toggle('show-links')
+})
